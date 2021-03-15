5a78c1c76fff3ba47bd2e88a6372008bcc08cd72
Abhishek <Abhishek@DESKTOP-3OC6S8L>
2021-03-15 14:12:44 +0530

    give the program

---
+++ ABHI123/src/test/java/com/crm/comcast/loginTest.java
@@ -0,0 +1,20 @@
+package com.crm.comcast;
+
+import org.testng.annotations.Test;
+
+public class loginTest {
+	
+	@Test
+	public void writing() {
+		System.out.println("my third program+");
+		
+	
+		
+		
+		
+		
+		
+		
+	}
+
+}
